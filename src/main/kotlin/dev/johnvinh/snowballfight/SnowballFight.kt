7deb8f0f2f7fd975c9b3286b4818b863fac1c8a6
John Vinh <john.michael.vinh2@gmail.com>
2023-01-02 17:32:55 -0500

    Add Kotlin to dependencies

---
+++ src/main/kotlin/dev/johnvinh/snowballfight/SnowballFight.kt
@@ -0,0 +1,13 @@
+package dev.johnvinh.snowballfight
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SnowballFight : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
