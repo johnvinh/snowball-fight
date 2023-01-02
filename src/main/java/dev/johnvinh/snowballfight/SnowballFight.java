f3c4191b1019610323d1eed0299323df2ee2252e
John Vinh <john.michael.vinh2@gmail.com>
2023-01-02 17:28:40 -0500

    Initial commit

---
+++ src/main/java/dev/johnvinh/snowballfight/SnowballFight.java
@@ -0,0 +1,17 @@
+package dev.johnvinh.snowballfight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SnowballFight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
