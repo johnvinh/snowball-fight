29fb28b31e9fbe87537e194f7e60be5fb4652902
John Vinh <john.michael.vinh2@gmail.com>
2023-01-03 10:49:01 -0500

    Create getters for arena corners to mark boundaries

--- src/main/kotlin/dev/johnvinh/snowballfight/ConfigManager.kt
+++ src/main/kotlin/dev/johnvinh/snowballfight/ConfigManager.kt
@@ -1,4 +1,26 @@
 package dev.johnvinh.snowballfight
 
-class ConfigManager {
+import org.bukkit.Location
+import org.bukkit.configuration.file.FileConfiguration
+
+class ConfigManager(private val snowballFight: SnowballFight) {
+    private val config: FileConfiguration = snowballFight.config
+
+    fun getArenaCorner1(): Location {
+        return Location(
+            config.getString("arena.world")?.let { snowballFight.server.getWorld(it) },
+            config.getDouble("arena.corner-1.x"),
+            config.getDouble("arena.corner-1.y"),
+            config.getDouble("arena.corner-1.z")
+        )
+    }
+
+    fun getArenaCorner2(): Location {
+        return Location(
+            config.getString("arena.world")?.let { snowballFight.server.getWorld(it) },
+            config.getDouble("arena.corner-2.x"),
+            config.getDouble("arena.corner-2.y"),
+            config.getDouble("arena.corner-2.z")
+        )
+    }
 }
