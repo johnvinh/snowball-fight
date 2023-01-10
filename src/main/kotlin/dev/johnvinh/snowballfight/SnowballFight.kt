4d600aa62b46cf43406aecfadc38066765bd1baf
John Vinh <john.michael.vinh2@gmail.com>
2023-01-10 18:43:44 -0500

    Initialize config manager

--- src/main/kotlin/dev/johnvinh/snowballfight/SnowballFight.kt
+++ src/main/kotlin/dev/johnvinh/snowballfight/SnowballFight.kt
@@ -3,6 +3,8 @@ package dev.johnvinh.snowballfight
 import org.bukkit.plugin.java.JavaPlugin
 
 class SnowballFight : JavaPlugin() {
+    val configManager: ConfigManager = ConfigManager(this)
+
     override fun onEnable() {
         // Plugin startup logic
     }
