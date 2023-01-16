54471634d4a890f26e39539deca9cb887b00f9c3
John Vinh <john.michael.vinh2@gmail.com>
2023-01-15 20:50:41 -0500

    Prevent block placing outside the arena

--- src/main/kotlin/dev/johnvinh/snowballfight/listeners/ArenaListener.kt
+++ src/main/kotlin/dev/johnvinh/snowballfight/listeners/ArenaListener.kt
@@ -5,6 +5,7 @@ import dev.johnvinh.snowballfight.SnowballFight
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
 
 class ArenaListener(private val plugin: SnowballFight) : Listener {
     private val cuboid: Cuboid = Cuboid(
@@ -13,6 +14,15 @@ class ArenaListener(private val plugin: SnowballFight) : Listener {
 
     @EventHandler
     fun onBlockBreak(e: BlockBreakEvent) {
+        // Don't allow players to break the arena walls or floor
+        if (!(cuboid.isIn(e.block.location))) {
+            e.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onBlockPlace(e: BlockPlaceEvent) {
+        // Don't allow players to place blocks outside the arena
         if (!(cuboid.isIn(e.block.location))) {
             e.isCancelled = true
         }
