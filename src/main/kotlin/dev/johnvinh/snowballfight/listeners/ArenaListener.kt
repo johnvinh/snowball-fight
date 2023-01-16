60529d83277ce45c0cc01b702584fa232b1516c6
John Vinh <john.michael.vinh2@gmail.com>
2023-01-15 20:43:06 -0500

    Start Arena Listener

---
+++ src/main/kotlin/dev/johnvinh/snowballfight/listeners/ArenaListener.kt
@@ -0,0 +1,20 @@
+package dev.johnvinh.snowballfight.listeners
+
+import dev.johnvinh.snowballfight.Cuboid
+import dev.johnvinh.snowballfight.SnowballFight
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+
+class ArenaListener(private val plugin: SnowballFight) : Listener {
+    private val cuboid: Cuboid = Cuboid(
+        plugin.configManager.getArenaCorner1(), plugin.configManager.getArenaCorner2()
+    )
+
+    @EventHandler
+    fun onBlockBreak(e: BlockBreakEvent) {
+        if (cuboid.isIn(e.block.location)) {
+            e.isCancelled = true
+        }
+    }
+}
